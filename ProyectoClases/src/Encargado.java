cdcf7df374153be519e42a6528b6e3d8b18d985a
LeslieLoaiza11 <leslielm63@gmail.com>
2020-05-07 09:12:04 -0500

    Parte de Leslie

---
+++ ProyectoClases/src/Encargado.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Leslie
+ */
+public class Encargado {
+    private String apellidos;
+    private String nombres;
+    private String correo;
+    private String telefono;
+
+    public Encargado(String apellidos, String nombres, String correo, String telefono) {
+        this.apellidos = apellidos;
+        this.nombres = nombres;
+        this.correo = correo;
+        this.telefono = telefono;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    
+    
+}
