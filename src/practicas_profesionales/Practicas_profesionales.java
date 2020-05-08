c679415649096466ddbcc565793c279753063801
= <=>
2020-05-08 00:11:33 -0500

    Agregué la carpeta libs

--- src/practicas_profesionales/Practicas_profesionales.java
+++ src/practicas_profesionales/Practicas_profesionales.java
@@ -19,7 +19,7 @@ public class Practicas_profesionales extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/views/AssignProject.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/views/StudentProgress.fxml"));
         
         Scene scene = new Scene(root);
         
